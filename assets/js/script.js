440506713098750a3b5d26b30394b56938274347
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-06 23:25:06 +0530

    Added Click event for ghosts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,25 @@
-function ghostOut() {
-    // ghosts[chooseRandomPumpkin()].css("top", "50%");
-    $(".ghost-"+chooseRandomPumpkin()).css("top","50%");
-    $(".ghost-"+chooseRandomPumpkin()).css("top","0%");
+let pumpkinNumber;
+
+function ghostAction() {
+    pumpkinNumber = chooseRandomPumpkin();
+    $(".ghost" + pumpkinNumber).css("top","0%");
+    setTimeout(ghostIn, 1500);
+}
+
+function ghostIn() {
+    $(".ghost" + pumpkinNumber).css("top","50%");
 }
 
 function chooseRandomPumpkin() {
-    return Math.floor(Math.random() * 5);
+    return Math.floor(Math.random() * 6);
 }
 
-setInterval(ghostOut, 1000);
+setInterval(ghostAction, 2000);
+
+$(".ghost").on("mousedown", function (event) {
+    let top = $(this).css("top");
+    console.log(top)
+    if (top == "0px"){
+        console.log("shot")
+    }
+});
