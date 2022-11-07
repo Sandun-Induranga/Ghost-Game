09d9d65e0320d8d3b5839db20ea0a7196d99fd43
Sandun-Induranga <sandutharu40@gmail.com>
2022-11-07 16:51:33 +0530

    Display Win

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ let pumpkinNumber;
 let up = false;
 const shotAudio = new Audio("https://rpg.hamsterrepublic.com/wiki-images/d/d7/Oddbounce.ogg");
 let clicked = false;
+let ariaValue = 100;
 
 function ghostAction() {
     clicked = false;
@@ -27,7 +28,30 @@ $(".ghost").on("mousedown", function () {
     if (top == "0px" && !clicked) {
         up = true;
         clicked = true;
-        $(".ghost").css("background"," url(assets/img/ghost-attacked.png)");
+        $(".ghost").css("background", "url(assets/img/ghost-attacked.png)");
+        ariaValue -= 10;
+        $(".progress-bar").attr("aria-valuenow", ariaValue);
+        $(".progress-bar").css("width", `${ariaValue}%`);
+        if (ariaValue == 0) {
+            $("main").css("display", `none`);
+            Swal.fire({
+                title: 'You Won',
+                text: "Go To Next Level",
+                icon: 'success',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire(
+                        'Deleted!',
+                        'Your file has been deleted.',
+                        'success'
+                    )
+                }
+            })
+        }
     }
 });
 
